2df29774da77b4f209c68f5476ed2326a3cc9c01
Lenin Tuasa <lenin.tuasaa@ug.edu.ec>
2023-11-09 20:13:29 -0500

    inicial

---
+++ src/app/pages/action-sheet/action-sheet.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ActionSheetPageRoutingModule } from './action-sheet-routing.module';
+
+import { ActionSheetPage } from './action-sheet.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ActionSheetPageRoutingModule
+  ],
+  declarations: [ActionSheetPage]
+})
+export class ActionSheetPageModule {}
